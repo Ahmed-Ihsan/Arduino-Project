35bed38d1708453cae2894751f953a7704edd08c
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2020-09-04 00:35:11 -0700

    test 1 with LED

---
+++ open the door.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+Servo servo;
+const int trigPin_1 = 11; // pin 11
+const int echoPin_1 = 10; // pin 10
+const int LED = 12; // pin 12
+int n = 100 ; //time = n x 100Ms
+int n1 = n;
+int m = 100 ; // distance 
+
+void setup() {
+  pinMode(trigPin_1, OUTPUT);
+  pinMode(echoPin_1, INPUT);
+  pinMode(LED, OUTPUT);
+} long duration_1, distance_1;
+
+void loop() {
+  sensor_sound_1(); // sensor on
+  digitalWrite(LED, LOW); // LED OFF
+  if (distance_1 < m) {
+    digitalWrite(LED, HIGH);  // LED ON
+    for (int i = 0; i < n1 ; n1--) { // timer >= 10s
+      sensor_sound_1();
+      delay(100);
+      if (distance_1 < m) {
+        n1 = n; 
+      }
+    }
+    n1=n; 
+    digitalWrite(LED,LOW); // relay off
+    delay(100); // for relay
+  }
+}
+
+
+void sensor_sound_1() {
+  digitalWrite(trigPin_1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin_1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin_1, LOW);
+  duration_1 = pulseIn(echoPin_1, HIGH);
+  distance_1 = duration_1 / 58.2;
+}
