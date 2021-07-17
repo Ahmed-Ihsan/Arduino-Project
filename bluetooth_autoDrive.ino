7d31c23c36abb9997cae71fbf4bc60a0a9ff78d4
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2021-07-17 13:55:16 +0300

    Update bluetooth_autoDrive.ino

--- bluetooth_autoDrive.ino
+++ bluetooth_autoDrive.ino
@@ -1,18 +1,18 @@
-#include <Servo.h>  
+#include <Servo.h>
 
-const int motor1 = 9;   
+const int motor1 = 9;
 const int motor_1 = 6;
 const int motor2 = 5;
 const int motor_2 = 3;
 
-int speed_=100;
+int speed_ = 100;
 int duration ;
 int distance ;
 int B = 0;
 int A = 0;
 char Incoming_value ;
-#define echoPin A2 
-#define trigPin A1 
+#define echoPin A2
+#define trigPin A1
 
 Servo servo_motor;
 
@@ -29,106 +29,107 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available() > 0){
+  
+  if (Serial.available() > 0) {
     Incoming_value = Serial.read();
     Serial.println(Incoming_value);
   }
+  
+  if (Incoming_value == 'B' || B ) {
+    B = 1;
+    A = 0;
+    speed_ = 255 ;
+    bletooth(Incoming_value);
+  }
 
-   if(Incoming_value == 'B' || B ){
-      B = 1;
-      A = 0;
-      speed_ = 255 ;
-      bletooth(Incoming_value);
-    }
-
-    if(Incoming_value == 'A' || A ){
+  if (Incoming_value == 'A' || A ) {
     speed_ = 100 ;
     auto_drive();
     B = 0;
     A = 1;
-    }
+  }
 
-}
-
-void auto_drive(){
   
 }
+void auto_drive() {
 
-void bletooth(char read_){
-  if(read_ == 'F'){
+}
+
+void bletooth(char read_) {
+  if (read_ == 'F') {
     forword();
-  }else if(read_ == 'X'){
+  } else if (read_ == 'X') {
     backword();
-  } else if(read_ == 'R'){
+  } else if (read_ == 'R') {
     right();
     forword();
-  }else if(read_ == 'L'){
+  } else if (read_ == 'L') {
     left();
     forword();
-  }else{
+  } else {
     stop_LR();
     stop_FB();
   }
 
 }
 
-int chack_L(){
+int chack_L() {
   servo_motor.write(150);
   delay(200);
   chack();
   return distance ;
 }
 
-int chack_F(){
+int chack_F() {
   servo_motor.write(98);
   delay(200);
   chack();
   return distance ;
 }
 
-int chack_R(){
+int chack_R() {
   servo_motor.write(42);
   delay(200);
   chack();
   return distance ;
 }
 
-void chack(){
+void chack() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin,HIGH);
+  duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
 }
 
-void forword(){
-  analogWrite(motor2,speed_);
-  analogWrite(motor_2,LOW);
+void forword() {
+  analogWrite(motor2, speed_);
+  analogWrite(motor_2, LOW);
 }
 
-void backword(){
-  analogWrite(motor2,LOW);
-  analogWrite(motor_2,speed_);
+void backword() {
+  analogWrite(motor2, LOW);
+  analogWrite(motor_2, speed_);
 }
 
-void right(){
-  analogWrite(motor1,HIGH);
-  analogWrite(motor_1,LOW);
+void right() {
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor_1, LOW);
 }
 
-void left(){
-  analogWrite(motor1,LOW);
-  analogWrite(motor_1,HIGH);
+void left() {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor_1, HIGH);
 }
 
-void stop_FB(){
-  analogWrite(motor2,LOW);
-  analogWrite(motor_2,LOW);
+void stop_FB() {
+  analogWrite(motor2, LOW);
+  analogWrite(motor_2, LOW);
 }
 
-void stop_LR(){
-  analogWrite(motor1,LOW);
-  analogWrite(motor_1,LOW);
+void stop_LR() {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor_1, LOW);
 }
