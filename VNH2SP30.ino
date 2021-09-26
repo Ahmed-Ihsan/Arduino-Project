b589dc3ccdc4213aafcd005dd3fb65222081d1d6
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2021-09-26 11:45:40 +0300

    Update VNH2SP30.ino

--- VNH2SP30.ino
+++ VNH2SP30.ino
@@ -1,35 +1,68 @@
-    /*
-  VNH2SP30-full-bridge-Driver
-  made on 01 Nov 2020
-  by Amir Mohammad Shojaee @ Electropeak
-  https://electropeak.com/learn/
+int PWM_1 = 3;
+int INA_1 = 2;
+int INB_1 = 4;
+int EN_1 = A0;
 
-*/
+int PWM_2 = 6;
+int INA_2 = 7;
+int INB_2 = 8;
+int EN_2 = A1;
 
-#define PWM 5
-#define INA 7
-#define INB 8
-
-#define EN A0
-
-int pot;
-int out1;
+int get_ = 0 ;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PWM,OUTPUT);
-  pinMode(INA,OUTPUT);
-  pinMode(INB,OUTPUT);
-  pinMode(EN,OUTPUT);
+  pinMode(PWM_1,OUTPUT);
+  pinMode(INA_1,OUTPUT);
+  pinMode(INB_1,OUTPUT);
+  pinMode(EN_1,OUTPUT);
+
+  pinMode(PWM_2,OUTPUT);
+  pinMode(INA_2,OUTPUT);
+  pinMode(INB_2,OUTPUT);
+  pinMode(EN_2,OUTPUT);
+  
 }
- 
+ void motor(int PWM ,int INA,int INB,int EN,int R,int speed_){
+  if (R == 1){
+    digitalWrite(INA,HIGH);
+    digitalWrite(INB,LOW);
+    }else if (R == -1){
+      digitalWrite(INA,LOW);
+      digitalWrite(INB,HIGH);
+    }else{
+      digitalWrite(INA,LOW);
+      digitalWrite(INB,LOW);
+    }
+
+    int pot;
+    int out1;
+    
+    digitalWrite(EN,HIGH);
+    out1=map(speed_,0,1023,0,255);
+    analogWrite(PWM,out1); 
+}
+
 void loop() {
-  
-  digitalWrite(INA,HIGH); //Motor A Rotate Clockwise
-  digitalWrite(INB,LOW);
-  digitalWrite(EN,HIGH);
-  
-  pot=analogRead(A5);
-  out1=map(pot,0,1023,0,255);
-  analogWrite(PWM,out1); //Speed control of Motor 
+  if (Serial.available() > 0){ 
+    get_ = Serial.read();
+    Serial.println(get_);
+    if(get_ == 119){
+      motor(PWM_1,INA_1,INB_1,EN_1,1,1023);
+      motor(PWM_2,INA_2,INB_2,EN_2,1,1023);
+    }else if(get_ == 115){
+      motor(PWM_1,INA_1,INB_1,EN_1,-1,1023);
+      motor(PWM_2,INA_2,INB_2,EN_2,-1,1023);
+    }else if(get_ == 100){
+      motor(PWM_1,INA_1,INB_1,EN_1,1,1023);
+      motor(PWM_2,INA_2,INB_2,EN_2,-1,1023);
+    }else if(get_ == 97){
+      motor(PWM_1,INA_1,INB_1,EN_1,-1,1023);
+      motor(PWM_2,INA_2,INB_2,EN_2,1,1023);
+    }else{
+      motor(PWM_1,INA_1,INB_1,EN_1,0,1023);
+      motor(PWM_2,INA_2,INB_2,EN_2,0,1023);
+    }
+  }
+
 }
