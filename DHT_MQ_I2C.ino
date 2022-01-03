6aaba5ba6fc8aae1ebbdda38d66b6c695dad9cc0
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2022-01-03 18:40:24 +0300

    Create DHT_MQ_I2C.ino

---
+++ DHT_MQ_I2C.ino
@@ -0,0 +1,50 @@
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN D4
+#define DHTTYPE    DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+int lcdColumns = 16;
+int lcdRows = 2;
+int smokeA0 = A0;
+int sensorThres = 600;
+int buzzer = D3;
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+void setup(){
+  lcd.init();                     
+  lcd.backlight();
+  pinMode(smokeA0, INPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop(){
+  int analogSensor = analogRead(smokeA0);
+  float newT = dht.readTemperature();
+  float newH = dht.readHumidity();
+  
+  lcd.setCursor(0, 0);
+  lcd.print("MQ : ");
+  lcd.print(analogSensor);
+  lcd.setCursor(0, 1);
+  lcd.print("T: ");
+  lcd.print(newT);
+  lcd.print(" -- ");
+  lcd.print("H: ");
+  lcd.print(newH);
+  
+  delay(1000);
+  lcd.clear(); 
+  if (analogSensor > sensorThres)
+ {
+   tone(buzzer, 1000, 200);
+ }
+ else
+ {
+   noTone(buzzer);
+ }
+}
