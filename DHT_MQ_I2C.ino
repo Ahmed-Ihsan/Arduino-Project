7257478639ca8de150b86f68cde2f79083413b79
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2022-01-13 22:53:49 +0300

    Update DHT_MQ_I2C.ino

--- DHT_MQ_I2C.ino
+++ DHT_MQ_I2C.ino
@@ -1,50 +1,191 @@
+#include <TinyGPS++.h> // library for GPS module
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
 
-#define DHTPIN D4
-#define DHTTYPE    DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
+TinyGPSPlus gps;  // The TinyGPS++ object
+SoftwareSerial ss(1, 3); // The serial connection to the GPS device
 
 int lcdColumns = 16;
 int lcdRows = 2;
-int smokeA0 = A0;
-int sensorThres = 600;
+
+const int trigPin = D6;
+const int echoPin = D7;
 int buzzer = D3;
+int led_g = D4;
+int led_r = D0;
 
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+#define SOUND_VELOCITY 0.034
+  
+const char* ssid = "TP-Link_6C72"; //ssid of your wifi
+const char* password = "56759860"; //password of your wifi
+float latitude , longitude;
+int year , month , date, hour , minute , second;
+String date_str , time_str , lat_str , lng_str;
+int pm;
+long duration;
+float distanceCm;
+float distanceInch;
 
-void setup(){
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
+WiFiServer server(80);
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(9600);
   lcd.init();                     
   lcd.backlight();
-  pinMode(smokeA0, INPUT);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password); //connecting to wifi
+  while (WiFi.status() != WL_CONNECTED)// while wifi not connected
+  {
+    delay(500);
+    Serial.print("."); //print "...."
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());  // Print the IP address
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   pinMode(buzzer, OUTPUT);
+  pinMode(led_g, OUTPUT);
+  pinMode(led_r, OUTPUT);
 }
 
-void loop(){
-  int analogSensor = analogRead(smokeA0);
-  float newT = dht.readTemperature();
-  float newH = dht.readHumidity();
+
+void loop()
+{
+  while (ss.available() > 0) //while data is available
+    if (gps.encode(ss.read())) //read gps data
+    {
+      if (gps.location.isValid()) //check whether gps location is valid
+      {
+        latitude = gps.location.lat();
+        lat_str = String(latitude , 6); // latitude location is stored in a string
+        longitude = gps.location.lng();
+        lng_str = String(longitude , 6); //longitude location is stored in a string
+      }
+      if (gps.date.isValid()) //check whether gps date is valid
+      {
+        date_str = "";
+        date = gps.date.day();
+        month = gps.date.month();
+        year = gps.date.year();
+        if (date < 10)
+        date_str = '0';
+        date_str += String(date);// values of date,month and year are stored in a string
+        date_str += " / ";
+
+        if (month < 10)
+          date_str += '0';
+        date_str += String(month); // values of date,month and year are stored in a string
+        date_str += " / ";
+        if (year < 10)
+          date_str += '0';
+        date_str += String(year); // values of date,month and year are stored in a string
+      }
+      if (gps.time.isValid())  //check whether gps time is valid
+      {
+        time_str = "";
+        hour = gps.time.hour();
+        minute = gps.time.minute();
+        second = gps.time.second();
+        minute = (minute + 30); // converting to IST
+        if (minute > 59)
+        {
+          minute = minute - 60;
+          hour = hour + 1;
+        }
+        hour = (hour + 5) ;
+        if (hour > 23)
+          hour = hour - 24;   // converting to IST
+        if (hour >= 12)  // checking whether AM or PM
+          pm = 1;
+        else
+          pm = 0;
+        hour = hour % 12;
+        if (hour < 10)
+          time_str = '0';
+        time_str += String(hour); //values of hour,minute and time are stored in a string
+        time_str += " : ";
+        if (minute < 10)
+          time_str += '0';
+        time_str += String(minute); //values of hour,minute and time are stored in a string
+        time_str += " : ";
+        if (second < 10)
+          time_str += '0';
+        time_str += String(second); //values of hour,minute and time are stored in a string
+        if (pm == 1)
+          time_str += " PM ";
+        else
+          time_str += " AM ";
+      }
+    }
+ 
+ WiFiClient client = server.available(); // Check if a client has connected
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>GPS DATA</title> <style>";
+  s += "a:link {background-color: YELLOW;text-decoration: none;}";
+  s += "table, th, td </style> </head> <body> <h1  style=";
+  s += "font-size:300%;";
+  s += " ALIGN=CENTER>Ahmed Ihsan</h1>";
+  s += "<p ALIGN=CENTER style=""font-size:150%;""";
+  s += "> <b>Location Details</b></p> <table ALIGN=CENTER style=";
+  s += "width:50%";
+  s += "> <tr> <th>Latitude</th>";
+  s += "<td ALIGN=CENTER >";
+  s += lat_str;
+  s += "</td> </tr> <tr> <th>Longitude</th> <td ALIGN=CENTER >";
+  s += lng_str;
+  s += "</td> </tr> <tr>  <th>Date</th> <td ALIGN=CENTER >";
+  s += date_str;
+  s += "</td></tr> <tr> <th>Time</th> <td ALIGN=CENTER >";
+  s += time_str;
+  s += "</td>  </tr> </table> ";
+ 
+  s += "</body> </html>";
+
+  client.print(s); // all the values are send to the webpage
   
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+ if (distanceCm < 8){
   lcd.setCursor(0, 0);
-  lcd.print("MQ : ");
-  lcd.print(analogSensor);
+  lcd.print("Distance(cm): ");
+  lcd.println(distanceCm);
   lcd.setCursor(0, 1);
-  lcd.print("T: ");
-  lcd.print(newT);
-  lcd.print(" -- ");
-  lcd.print("H: ");
-  lcd.print(newH);
-  
-  delay(1000);
-  lcd.clear(); 
-  if (analogSensor > sensorThres)
- {
-   tone(buzzer, 1000, 200);
+  lcd.print("Box is full ---");
+  digitalWrite(led_g,LOW);
+  digitalWrite(led_r,HIGH);
+  tone(buzzer, 1000, 200);
+  delay(500);
+  noTone(buzzer);
+  delay(500);
+ }else{
+  lcd.setCursor(0, 0);
+  lcd.print("Distance(cm): ");
+  lcd.println(distanceCm);
+  lcd.setCursor(0, 1);
+  lcd.print("Box is not full");
+  noTone(buzzer);
+  digitalWrite(led_g,HIGH);
+  digitalWrite(led_r,LOW);
  }
- else
- {
-   noTone(buzzer);
- }
+
+delay(100);
 }
