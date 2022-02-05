1062fc03dab694ccf14ee6e86333927150afa83e
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2022-02-05 20:56:46 +0300

    Update DHT_MQ_I2C.ino

--- DHT_MQ_I2C.ino
+++ DHT_MQ_I2C.ino
@@ -1,3 +1,6 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
@@ -12,24 +15,41 @@ int lcdRows = 2;
 int smokeA0 = A0;
 int sensorThres = 600;
 int buzzer = D3;
+int fi = D5;
 
+char auth[] = "Usg7C01eJO60JVSsggl5yEJ_CDbq3Xz3";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "TP-Link_6C72";
+char pass[] = "56759860";
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
-void setup(){
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(D3,OUTPUT);
   lcd.init();                     
   lcd.backlight();
   pinMode(smokeA0, INPUT);
   pinMode(buzzer, OUTPUT);
+  pinMode(fi, INPUT);
+  Blynk.begin(auth, ssid, pass);
 }
 
-void loop(){
+void loop()
+{
+ 
   int analogSensor = analogRead(smokeA0);
+  int fier = digitalRead(fi);
   float newT = dht.readTemperature();
   float newH = dht.readHumidity();
-
   lcd.setCursor(0, 0);
   lcd.print("MQ : ");
   lcd.print(analogSensor);
+  if(fier){
+    lcd.print(" -- fier");
+  }
   lcd.setCursor(0, 1);
   lcd.print("T: ");
   lcd.print(newT);
