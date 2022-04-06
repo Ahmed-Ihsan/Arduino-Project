60094fac09379986f543716e39e93235dbfd0abe
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2022-04-06 23:08:28 +0300

    Create car_esp.ino

--- car_esp.ino
+++ car_esp.ino
@@ -1,80 +1,78 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
 
-// Motor A
-const int motor1 = 36;
-const int motor_1 = 39;
-const int motor2 = 34;
-const int motor_2 = 35;
-const int speed_pin1 = 32;
-const int speed_pin2 = 33;
+int motor1Pin_1 = 32; 
+int motor1Pin_2 = 33; 
+
+int enable1Pin1 = 14;
+int enable1Pin2 = 25; 
 
 // Setting PWM properties
 const int freq = 30000;
-const int pwmChannel = 0;
+const int pwmChannel0 = 0;
+const int pwmChannel1 = 1;
+
 const int resolution = 8;
-int dutyCycle = 100;
+int dutyCycle = 200;
 
 void setup() {
-  // sets the pins as outputs:
-  pinMode(motor1, OUTPUT);
-  pinMode(motor_1, OUTPUT);
-  pinMode(motor2, OUTPUT);
-  pinMode(motor_2, OUTPUT);
-  
-  // configure LED PWM functionalitites
-  ledcSetup(pwmChannel, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(speed_pin1, pwmChannel);
-  ledcAttachPin(speed_pin2, pwmChannel);
- 
-  ledcWrite(pwmChannel, dutyCycle);
-  
-  Serial.begin(115200);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin1, OUTPUT);
 
-  // testing
+  pinMode(motor1Pin_1, OUTPUT);
+  pinMode(motor1Pin_2, OUTPUT);
+  pinMode(enable1Pin2, OUTPUT);
+ 
+  ledcSetup(pwmChannel0, freq, resolution);
+  ledcSetup(pwmChannel1, freq, resolution);
+  
+  ledcAttachPin(enable1Pin1, pwmChannel0);
+  ledcAttachPin(enable1Pin2, pwmChannel1);
+
+  ledcWrite(pwmChannel0, dutyCycle);
+  ledcWrite(pwmChannel1, dutyCycle);
+  Serial.begin(115200);
   Serial.print("Testing DC Motor...");
 }
 
 void loop() {
-  
-forword();
-}
-
-void left() {
-  digitalWrite(motor2, HIGH);
-  digitalWrite(motor_2, LOW);
-  digitalWrite(motor1, HIGH);
-  digitalWrite(motor_1, LOW);
-}
-
-void right() {
-  digitalWrite(motor2, LOW);
-  digitalWrite(motor_2, HIGH);
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor_1, HIGH);
-}
-
-void backword() {
-  digitalWrite(motor1, HIGH);
-  digitalWrite(motor_1, LOW);
-  digitalWrite(motor2, LOW);
-  digitalWrite(motor_2, HIGH);
+  left();
 }
 
 void forword() {
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor_1, HIGH);
-  digitalWrite(motor2, HIGH);
-  digitalWrite(motor_2, LOW);
+  digitalWrite(motor1Pin_1, HIGH);
+  digitalWrite(motor1Pin_2, LOW); 
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  
+}
+
+void backword() { 
+  digitalWrite(motor1Pin_1, LOW);
+  digitalWrite(motor1Pin_2, HIGH); 
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+}
+
+void right() {
+  digitalWrite(motor1Pin_1, HIGH);
+  digitalWrite(motor1Pin_2, LOW); 
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+}
+
+void left() {
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin_1, LOW);
+  digitalWrite(motor1Pin_2, HIGH); 
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
 }
 
 void Break() {
-  digitalWrite(motor2, HIGH);
-  digitalWrite(motor_2, HIGH);
-  digitalWrite(motor1, HIGH);
-  digitalWrite(motor_1, HIGH);
+  digitalWrite(motor1Pin_1, LOW);
+  digitalWrite(motor1Pin_2, LOW); 
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW); 
 }
